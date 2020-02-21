3930d814bd311f764bbf63cd2d4080ccfd5d4a16
simonleang <simonleang@foxmail.com>
2020-02-22 01:12:58 +0800

    crete serializer for hello-view

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profile_api import serializers
 
 
 class HelloApiView(APIView):
     """dtest Api View."""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """return a list of APIView features"""
@@ -15,3 +19,15 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiView': an_apiView})
+
+    def post(self, request):
+        """create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+
+            message = f'Hello, {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
