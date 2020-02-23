109f5af578354ab374d91ac8a55ec3f0e300f645
simonleang <simonleang@foxmail.com>
2020-02-23 14:17:36 +0800

    add allowed hosts

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/simon-leang/my-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/my-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
